7f5ba3730b6ef863231643467e4b4595bccbf9fa
Aashishkp <ping.aashishkp@gmail.com>
2024-01-23 21:10:06 +0530

    New

--- Personal/Test.java
+++ Personal/Test.java
@@ -8,6 +8,27 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws Exception {
+    	//Write a  Program To Print All Possible Substrings Of A String : java
     	
+    	String inputString = "java";
+    	for(int i = 0;i<inputString.length();i++) {
+    		
+    		for(int j=i+1; j<=inputString.length();j++){
+    			
+    			System.out.println(inputString.substring(i,j));
+    		}
+    	}	
 	}
 }
+
+
+J
+Ja
+Jav
+Java
+a
+av
+ava
+v
+va
+a
